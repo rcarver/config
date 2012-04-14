924f912a556960cb8c6f42e303c902ee946b0541
Ryan Carver <ryan@ryancarver.com>
2012-04-14 09:27:06 -0700

    doc

--- lib/config/patterns.rb
+++ lib/config/patterns.rb
@@ -1,9 +1,28 @@
 module Config
+  # This module provides helper methods for common patterns.
+  # These helpers are available in Blueprints and Patterns.
+  #
+  # Examples
+  #
+  #     # Instead of this.
+  #     add Config::Patterns::File do |f|
+  #       f.path = "/tmp/file"
+  #     end
+  #
+  #     # You can do this.
+  #     file "/tmp/file"
+  #
+  #     # To configure more attributes, you can still use a block.
+  #     file "/tmp/file" do |f|
+  #       f.owner = "www"
+  #     end
+  #
   module Patterns
 
-    autoload :Directory, 'config/patterns/directory'
-    autoload :File, 'config/patterns/file'
-
+    # Public: Add a file.
+    #
+    # path - String path of the file.
+    #
     def file(path)
       add Config::Patterns::File do |p|
         p.path = path
@@ -11,11 +30,20 @@ module Config
       end
     end
 
+    # Public: Add a directory.
+    #
+    # path - String path of the directory.
+    #
     def dir(path, &block)
       add Config::Patterns::Directory do |p|
         p.path = path
         yield p if block_given?
       end
     end
+
+    # Autoload builtin patterns.
+
+    autoload :Directory, 'config/patterns/directory'
+    autoload :File, 'config/patterns/file'
   end
 end
