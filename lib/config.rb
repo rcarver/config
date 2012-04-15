26d6613228277dd59973667f26da63998e76cb8e
Ryan Carver <ryan@ryancarver.com>
2012-04-14 18:45:17 -0700

    docs

--- lib/config.rb
+++ lib/config.rb
@@ -26,17 +26,27 @@ require 'config/meta'
 
 module Config
 
+  # Public: Get the the global logger.
+  #
+  # Returns a Config::Log.
   def self.log
     @log ||= Config::Log.new
   end
 
+  # Public: Initialize a new logger using the given stream.
+  #
+  # stream - An IO object.
+  #
+  # Returns nothing.
   def self.log_to(stream)
     @log = Config::Log.new(stream)
   end
 
+  # Public: Create an execute a Blueprint with a block.
+  #
+  # Returns nothing.
   def self.blueprint(&block)
     blueprint = Config::Blueprint.new('tmp', &block)
     blueprint.execute
   end
-
 end
