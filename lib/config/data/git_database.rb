7a5da87a648c0fd2762005880a8292c7c43f4736
Ryan Carver <ryan@ryancarver.com>
2012-05-12 10:00:36 -0700

    docs

--- lib/config/data/git_database.rb
+++ lib/config/data/git_database.rb
@@ -7,6 +7,11 @@ module Config
         @repo = repo
       end
 
+      # Store information about a node in the database.
+      #
+      # node - Config::Node.
+      #
+      # Returns nothing.
       def update_node(node)
         file = facts_file(node)
         status = file.exist? ? "Updated" : "Added"
@@ -21,6 +26,11 @@ module Config
         end
       end
 
+      # Remove information about a node from the databse.
+      #
+      # node - Config::Node.
+      #
+      # Returns nothing.
       def remove_node(node)
         return if !facts_file(node).exist?
 
@@ -36,13 +46,22 @@ module Config
         @path + "facts/#{node.fqn}.json"
       end
 
+      # This is a critically important function. Here we make sure that changes
+      # to the git repo are made cleanly and pushed to the origin.
       def txn
+        # Do a hard reset on the repository to ensure a clean slate.
         @repo.reset_hard
+        # The caller should make changes and commit them.
         yield
         begin
+          # Attempt to push to the origin. If an error occurs,
+          # pull from the origin and rebase our local changes.
+          # Then try to push again.
           @repo.push
-        rescue Config::Data::Repo::PushError
+        rescue Config::Data::Repo::PushError => e
           @repo.pull_rebase
+          # NOTE: We should determine if retries should be slower
+          # or if a backoff strategy should be designed.
           retry
         end
       end
