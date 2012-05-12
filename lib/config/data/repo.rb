21c8673f12ff3ff4de2898a95c565d167a0f688a
Ryan Carver <ryan@ryancarver.com>
2012-05-11 21:52:20 -0700

    functional git client

--- lib/config/data/repo.rb
+++ lib/config/data/repo.rb
@@ -1,13 +1,37 @@
 module Config
   module Data
+    # A very simple git client. I have chosen to implement my own client
+    # rather than use an existing library in order to 1) reduce
+    # dependencies, 2) provide complete clarity on how the data repo is
+    # maintained.
     class Repo
+
+      StateError  = Class.new(RuntimeError)
+      CommitError = Class.new(StateError)
+      PushError   = Class.new(StateError)
+
+      class Cmd
+
+        Failure = Class.new(RuntimeError)
+
+        def initialize(dir)
+          @dir = dir
+        end
+
+        def run(command)
+          ::Dir.chdir(@dir) {
+            out, s = Open3.capture2e(command)
+            raise Failure, out unless s.exitstatus == 0
+          }
+        end
+      end
 
       def initialize(dir, name)
         @dir = dir
         @name = name
         @path = Pathname.new(@dir) + name
-        @clone = Config::Core::Cmd.new(@dir)
-        @git = Config::Core::Cmd.new(@path)
+        @clone = Cmd.new(@dir)
+        @git = Cmd.new(@path)
       end
 
       attr :path
@@ -15,28 +39,68 @@ module Config
       attr_writer :git
       attr_writer :clone
 
+      # Clone a git repo.
+      #
+      # url - String url to clone.
+      #
+      # Returns nothing.
       def clone(url)
         @clone.run "git clone #{url} #{@name}"
       end
 
-      def reset
-        @git.run "git reset --hard"
-      end
-
+      # Pull from origin. Performs a pull with rebase to minimize merge
+      # noise.
+      #
+      # Returns nothing.
       def pull
-        @git.run "git pull"
+        @git.run "git pull --rebase"
       end
 
+      # Add files to the index.
+      #
+      # Returns nothing.
       def add(path)
         @git.run "git add #{path}"
       end
 
+      # Remove files from the index.
+      #
+      # Returns nothing.
       def rm(path)
         @git.run "git rm #{path}"
       end
 
+      # Reset the index. Does a hard reset to ensure a clean slate.
+      #
+      # Returns nothing.
+      def reset
+        @git.run "git reset --hard"
+      end
+
+      # Commit a change.
+      #
+      # Returns nothing.
+      # Raises CommitError if there is a problem, most likely that there
+      # is nothing to commit.
       def commit(msg)
-        @git.run "git commit -m '#{msg}'"
+        begin
+          @git.run "git commit -m '#{msg}'"
+        rescue Cmd::Failure => e
+          raise CommitError, e.message
+        end
+      end
+
+      # Push commits to the origin.
+      #
+      # Returns nothing.
+      # Raises PushError if there is a problem, most likely that there
+      # are changes to pull.
+      def push
+        begin
+          @git.run "git push origin HEAD"
+        rescue Cmd::Failure => e
+          raise PushError, e.message
+        end
       end
 
     end
--- test/slow/repo_test.rb
+++ test/slow/repo_test.rb
@@ -2,28 +2,136 @@ require 'helper'
 
 describe "filesystem", Config::Data::Repo do
 
-  let(:source_repo) { tmpdir + "source.git" }
-  let(:project_repo) { tmpdir + "project" }
+  def within(dir, &block)
+    Dir.chdir(dir, &block)
+  end
 
-  subject { Config::Data::Repo.new(tmpdir, "project") }
+  def cmd(command)
+    o, s = Open3.capture2e(command)
+    raise o unless s.exitstatus == 0
+    o
+  end
 
-  def create_repo
-    source_repo.mkdir
-    Dir.chdir(source_repo) do
-      `git init .`
-      `echo 'hello' > README.md`
-      `git add README.md`
-      `git commit -m 'hi'`
+  describe "working with the index" do
+
+    let(:project_dir) { tmpdir + "project" }
+
+    subject { Config::Data::Repo.new(tmpdir, "project") }
+
+    before do
+      project_dir.mkdir
+      within(project_dir) do
+        cmd "git init ."
+        cmd "echo hello > README.md"
+        cmd "git add README.md"
+        cmd "git commit -m 'one'"
+      end
+    end
+
+    describe "#add" do
+
+      before do
+        within(project_dir) { `echo world >> README.md` }
+      end
+
+      it "stages changes to file" do
+        subject.add "."
+        within(project_dir) do
+          cmd("git status") .must_include "Changes to be committed"
+        end
+      end
+    end
+
+    describe "#rm" do
+
+      before do
+        subject.rm "README.md"
+      end
+
+      it "stages removal of a file" do
+        within(project_dir) do
+          cmd("git status") .must_include "Changes to be committed"
+        end
+      end
+    end
+
+    describe "#reset" do
+
+      before do
+        within(project_dir) { `echo world >> README.md` }
+      end
+
+      it "stages changes to file" do
+        subject.reset
+        within(project_dir) do
+          cmd("git status").must_include "nothing to commit"
+        end
+      end
+    end
+
+    describe "#commit" do
+
+      before do
+        within(project_dir) { `echo world >> README.md` }
+      end
+
+      it "fails if there is nothing to commit" do
+        proc { subject.commit "okok" }.must_raise Config::Data::Repo::CommitError
+      end
+
+      it "commits changes" do
+        subject.add "."
+        subject.commit "okok"
+        within(project_dir) do
+          cmd("git status").must_include "nothing to commit"
+        end
+      end
     end
   end
 
-  describe "#clone" do
-    it "clones an existing repo" do
-      create_repo
-      project_repo.wont_be :exist?
-      subject.clone(source_repo)
-      project_repo.must_be :exist?
-      (project_repo + "README.md").read.must_equal "hello\n"
+  describe "working with remotes" do
+
+    let(:source_dir) { tmpdir + "source.git" }
+    let(:project1_dir) { tmpdir + "project1" }
+    let(:project2_dir) { tmpdir + "project2" }
+
+    let(:project1) { Config::Data::Repo.new(tmpdir, "project1") }
+    let(:project2) { Config::Data::Repo.new(tmpdir, "project2") }
+
+    it "works" do
+      source_dir.mkdir
+      within(source_dir) do
+        cmd "git init --bare"
+      end
+
+      # clone source to project1
+      project1_dir.wont_be :exist?
+      project1.clone(source_dir)
+      project1_dir.must_be :exist?
+
+      # clone source to project2
+      project2_dir.wont_be :exist?
+      project2.clone(source_dir)
+      project2_dir.must_be :exist?
+
+      # commit and push from project1
+      within(project1_dir) { cmd "echo hello > one" }
+      project1.add "."
+      project1.commit "from 1"
+      project1.push
+
+      # commit and push from project2 with state resolution
+      within(project2_dir) { cmd "echo goodbye > two" }
+      project2.add "."
+      project2.commit "from 2"
+      proc { project2.push }.must_raise Config::Data::Repo::PushError
+      project2.pull
+      (project2_dir + "one").must_be :exist?
+      project2.push
+
+      # pull changes into project1
+      project1.pull
+      (project2_dir + "two").must_be :exist?
     end
   end
 end
