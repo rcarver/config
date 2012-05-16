b55a58e92de0f17dbf0278bf3b3a47656fb627bc
Ryan Carver <ryan@ryancarver.com>
2012-05-16 07:34:04 -0700

    improve Node to input/output JSON

--- lib/config/node.rb
+++ lib/config/node.rb
@@ -1,7 +1,18 @@
 module Config
   class Node
 
+    def self.from_json(json)
+      node = new(
+        json["cluster"],
+        json["blueprint"],
+        json["identity"]
+      )
+      node.facts = Config::Core::Facts.from_json(json["facts"])
+      node
+    end
+
     def initialize(cluster_name, blueprint_name, identity)
+      # TODO: enforce that none of the args may contain a dash.
       @cluster_name = cluster_name
       @blueprint_name = blueprint_name
       @identity = identity
@@ -13,6 +24,16 @@ module Config
 
     attr_accessor :facts
 
+    # Public: Get the fully qualified node name. This name
+    # is used as the `hostname` of a node in order to identify it
+    # consistently thoughout the system.
+    #
+    # Examples
+    #
+    #   node = Node.new("prod", "webserver", "ix88")
+    #   node.fqn # => "prod-webserver-ix88"
+    #
+    # Returns a String.
     def fqn
       [cluster_name, blueprint_name, identity].join('-')
     end
@@ -22,7 +43,7 @@ module Config
         cluster: cluster_name.to_s,
         blueprint: blueprint_name.to_s,
         identity: identity.to_s,
-        facts: facts || {}
+        facts: facts ? facts.as_json : {}
       }
     end
 
--- test/config/node_test.rb
+++ test/config/node_test.rb
@@ -2,22 +2,39 @@ require 'helper'
 
 describe Config::Node do
 
-  let(:cluster_name)   { :production }
-  let(:blueprint_name) { :webserver }
+  let(:cluster_name)   { "production" }
+  let(:blueprint_name) { "webserver" }
   let(:identity)       { "xyz" }
 
+  let(:facts_data) { 
+    { "ec2" => { "public_ipv4" => "127.0.0.1" } }
+  }
+
   subject { Config::Node.new(cluster_name, blueprint_name, identity) }
+
+  before do
+    subject.facts = Config::Core::Facts.new(facts_data)
+  end
 
   specify "#fqn" do
     subject.fqn.must_equal "production-webserver-xyz"
   end
 
   specify "#as_json" do
-    subject.as_json.must_equal({
-      cluster: "production",
-      blueprint: "webserver",
-      identity: "xyz",
-      facts: {}
-    })
+    subject.as_json.must_equal(
+      cluster: cluster_name,
+      blueprint: blueprint_name,
+      identity: identity,
+      facts: facts_data
+    )
+  end
+
+  specify ".from_json" do
+    json = JSON.load(JSON.dump(subject.as_json))
+    node = Config::Node.from_json(json)
+    node.cluster_name.must_equal "production"
+    node.blueprint_name.must_equal "webserver"
+    node.identity.must_equal "xyz"
+    node.facts.ec2.public_ipv4.must_equal "127.0.0.1"
   end
 end
