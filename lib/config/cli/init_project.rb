029e3c5781999fbe12c64ea1c6002de0995a3858
Ryan Carver <ryan@ryancarver.com>
2012-07-11 07:55:38 -0400

    update project initialization to add smart git defaults

--- lib/config/cli/init_project.rb
+++ lib/config/cli/init_project.rb
@@ -11,12 +11,22 @@ Generate the template for a new project.
       end
 
       def execute
+        default_remotes = remotes_factory.call
         blueprint do
           add Config::Meta::Project do |p|
             p.root = Dir.pwd
+            p.project_hostname_domain = "internal.example.com"
+            p.project_git_config_url = default_remotes.project_git_config.url
+            p.database_git_config_url = default_remotes.database_git_config.url
           end
         end
       end
+
+      def remotes_factory
+        @remotes_factory or -> { Config::Core::Remotes.default(project, project_data) }
+      end
+
+      attr_writer :remotes_factory
 
     end
   end
