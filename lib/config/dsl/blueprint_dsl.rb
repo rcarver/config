698ce2afb0f68537e0fa526c80e540f420f5501a
Ryan Carver <ryan@ryancarver.com>
2012-07-25 21:37:48 -0700

    fix cluster docs

--- lib/config/dsl/blueprint_dsl.rb
+++ lib/config/dsl/blueprint_dsl.rb
@@ -2,17 +2,17 @@ module Config
   module DSL
     # Syntax for Blueprint files, stored at blueprints/[name].rb.
     # Syntactically, a Blueprint is a special Pattern from which you may
-    # access the current Node and current Cluster.
+    # access the current node, cluster and merge configuration.
     class BlueprintDSL < Config::Pattern
 
-      # Public: Get the current node.
+      # Public: Get the current node facts.
       #
-      # Returns a Config::Node.
+      # Returns a Config::Core::Facts.
       attr_reader :node
 
       # Public: Get the current configuration.
       #
-      # Returns a Config::Core::Variables.
+      # Returns a Config::Configuration.
       attr_reader :configure
 
       def to_s
