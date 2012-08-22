8b0f3c0c24b8e4a02bc5ca2ed734a2bf19036c13
Ryan Carver <ryan@ryancarver.com>
2012-08-22 08:52:25 -0700

    add indenting and redefine `dent` to be de-dent and then in-dent

--- lib/config/core_ext/string_dent.rb
+++ lib/config/core_ext/string_dent.rb
@@ -1,8 +1,9 @@
 class String
 
-  # Public: De-dent a string. Removes leading whitespace from each line of a
-  # string by removing the smallest amount of leading whitespace found on
-  # any line of the string.
+  # Public: De-dent and in-dent a string. With no arguments, de-dents. With an
+  # argument, de-dents and then in-dents.
+  #
+  # indent_level - String or Integer used to ident (default: no indent).
   #
   # Examples
   #
@@ -12,18 +13,76 @@ class String
   #     STR
   #     str.inspect # => "First Line\n  Indented Line\n"
   #
-  #     str = <<-STR.dent
+  #     str = <<-STR.dent(2)
+  #         First Line
+  #           Indented Line
+  #     STR
+  #     str.inspect # => "  First Line\n    Indented Line\n"
+  #
+  # Returns a String.
+  def dent(indent_level = nil)
+    if indent_level
+      dedent.indent(indent_level)
+    else
+      dedent
+    end
+  end
+
+  # Public: De-dent a string. Removes leading whitespace from each line of a
+  # string by removing the smallest amount of leading whitespace found on
+  # any line of the string.
+  #
+  # Examples
+  #
+  #     str = <<-STR.dedent
+  #       First Line
+  #         Indented Line
+  #     STR
+  #     str.inspect # => "First Line\n  Indented Line\n"
+  #
+  #     str = <<-STR.dedent
   #         Indented Line
   #       Second Line
   #     STR
   #     str.inspect # => "  Indented Line\nSecond Line\n"
   #
   # Returns a String.
-  def dent
+  def dedent
     lines = split("\n")
     head = lines.map { |line| line[/\A\s*/] }.min
     tail = self[/\n*\Z/]
     dented = lines.map { |line| line.sub(/\A#{head}/, '') }
     dented.join("\n") + tail
+  end
+
+  # Public: In-dent a string. Addes leading whitespace to each line.
+  #
+  # level - String or Integer used to indent. A String is used as is,
+  #         an integer indicates how many spaces to indent.
+  #
+  # Examples
+  #
+  #   str = <<-STR.indent(2)
+  #   First Line
+  #     Second Line
+  #   STR
+  #   str.inspect # => "  First Line\n    Second Line\n"
+  #
+  #   str = <<-STR.indent("* ")
+  #   First Line
+  #     Second Line
+  #   STR
+  #   str.inspect # => "* First Line\n*   Second Line\n"
+  #
+  # Returns a String.
+  def indent(level)
+    level_str = case level
+    when String then level
+    when Fixnum then " " * level
+    else raise ArgumentError, "#{level.inspect} is not a valid indent level"
+    end
+    lines = split("\n")
+    tail = self[/\n*\Z/]
+    lines.map { |line| "#{level_str}#{line}" }.join("\n") + tail
   end
 end
--- test/config/core_ext/string_dent_test.rb
+++ test/config/core_ext/string_dent_test.rb
@@ -2,13 +2,34 @@ require 'helper'
 
 describe String, "#dent" do
 
+  it "de-dents" do
+    str = "  Hello"
+    str.dent.must_equal "Hello"
+  end
+
+  it "in-dents" do
+    str = "Hello"
+    str.dent(2).must_equal "  Hello"
+  end
+
+  it "de-dents and in-dents" do
+    str = <<-STR.dent(2)
+      One
+        Two
+    STR
+    str.dent(2).must_equal "  One\n    Two\n"
+  end
+end
+
+describe String, "#dedent" do
+
   it "de-dents a multi-line string where the indent level is on the first line" do
     str = <<-STR
       One
         Two
           Three
     STR
-    str.dent.must_equal "One\n  Two\n    Three\n"
+    str.dedent.must_equal "One\n  Two\n    Three\n"
   end
 
   it "de-dents a multi-line string where the shortest indent is on a later line" do
@@ -17,28 +38,28 @@ describe String, "#dent" do
         Two
       Three
     STR
-    str.dent.must_equal "    One\n  Two\nThree\n"
+    str.dedent.must_equal "    One\n  Two\nThree\n"
   end
 
   it "de-dents a single-line string" do
     str = "  Hello"
-    str.dent.must_equal "Hello"
+    str.dedent.must_equal "Hello"
   end
 
   it "does not replace whitespace within the string" do
     str = "  Hello  World  "
-    str.dent.must_equal "Hello  World  "
+    str.dedent.must_equal "Hello  World  "
   end
 
   it "does nothing if there is no indent" do
     str = "Hello"
-    str.dent.must_equal "Hello"
+    str.dedent.must_equal "Hello"
   end
 
   it "matches the trailing newlines of the original string" do
-    "a".dent.must_equal "a"
-    "a\n".dent.must_equal "a\n"
-    "a\n\n".dent.must_equal "a\n\n"
+    "a".dedent.must_equal "a"
+    "a\n".dedent.must_equal "a\n"
+    "a\n\n".dedent.must_equal "a\n\n"
   end
 
   it "initial blank lines don't cause denting" do
@@ -47,6 +68,39 @@ describe String, "#dent" do
       Hello
       World
     STR
-    str.dent.must_equal "\n      Hello\n      World\n"
+    str.dedent.must_equal "\n      Hello\n      World\n"
   end
+end
+
+describe String, "#indent" do
+
+  it "indents each line of a multiline string" do
+    str = <<-STR
+One
+  Two
+    STR
+    str.indent(2).must_equal "  One\n    Two\n"
+  end
+
+  it "indents with a string" do
+    str = "Hello"
+    str.indent("x ").must_equal "x Hello"
+  end
+
+  it "indents with an integer" do
+    str = "Hello"
+    str.indent(4).must_equal "    Hello"
+  end
+
+  it "cannot indent with anything else" do
+    str = "Hello"
+    -> { str.indent(3.3) }.must_raise ArgumentError
+  end
+
+  it "matches the trailing newlines of the original string" do
+    "a".indent(2).must_equal "  a"
+    "a\n".indent(2).must_equal "  a\n"
+    "a\n\n".indent(2).must_equal "  a\n\n"
+  end
+
 end
