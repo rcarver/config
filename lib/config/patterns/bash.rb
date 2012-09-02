78875798d735f2dacac228a65f462cdeef7419ab
Ryan Carver <ryan@ryancarver.com>
2012-09-01 20:52:18 -0700

    Bash now uses program args to set -e -u

--- lib/config/patterns/bash.rb
+++ lib/config/patterns/bash.rb
@@ -5,6 +5,9 @@ module Config
       desc { Config::Patterns::Script[:name].desc }
       key :name
 
+      desc { Config::Patterns::Script[:args].desc }
+      attr :args, ["-e", "-u"]
+
       desc { Config::Patterns::Script[:code].desc }
       attr :code
 
@@ -14,9 +17,6 @@ module Config
       desc { Config::Patterns::Script[:not_if].desc }
       attr :not_if, nil
 
-      desc "Set sensible default options (-e and -u)"
-      attr :set_sensible_defaults, true
-
       def describe
         "Bash #{name.inspect}"
       end
@@ -24,23 +24,11 @@ module Config
       def call
         add Config::Patterns::Script do |s|
           s.name = name
-          s.code = add_sensible_defaults(code)
-          s.reverse = add_sensible_defaults(reverse)
+          s.open = "bash"
+          s.args = args
+          s.code = code
+          s.reverse = reverse
           s.not_if = not_if
-        end
-      end
-
-    protected
-
-      def add_sensible_defaults(script)
-        if set_sensible_defaults
-          [
-            "set -o nounset",
-            "set -o errexit",
-            script
-          ].join("\n")
-        else
-          script
         end
       end
 
