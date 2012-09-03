a765b4b22948d67652d4b5c0f5e07d5254b6c4fe
Ryan Carver <ryan@ryancarver.com>
2012-09-03 15:54:55 -0700

    docs for Script

--- lib/config/patterns/script.rb
+++ lib/config/patterns/script.rb
@@ -1,5 +1,36 @@
 module Config
   module Patterns
+    # A low level pattern. The Script pattern provides complete control
+    # over how a system command is executed. It allows commands to be
+    # executed for three different reasons:
+    #
+    #   1. `code` - Code to execute on the system in order to reach the
+    #   desired state. This code should be idempotent itself, or be
+    #   idempotent with the addition of "not_if" code. If this code
+    #   exits with a non-zero status it is considered an error and the
+    #   Config execution will abort.
+    #
+    #   2. `not_if` - Code to execute in order to determine if `code`
+    #   should execute. Use this to check for the state that `code` is
+    #   expected to create. For example, if `code` creates a file on
+    #   disk, `not_if` should check for the existence of that file. If
+    #   `not_if` returns a zero status then `code` will not be executed.
+    #   Any other status will result in `code` being run.
+    #
+    #   3. `reverse` - Code to execute in order to reverse the effects
+    #   of `code`. When the pattern is destroyed, the `reverse` code
+    #   will be executed. If the reverse code exits with a non-zero
+    #   status it is considered an error and the Config execution will
+    #   abort.
+    #
+    # By default, everything is executed with the `sh` interpreter with
+    # no arguments. It is recommended to use a higher level pattern such
+    # as `Config::Patterns::Bash` for shell scripts, or a
+    # language-specific pattern for executing other types of code.
+    #
+    # Note that the interpeter set via `code_exec` will also determine
+    # the intepreter for `not_if_exec` and `reverse_exec` if they are
+    # not explicitly set.
     class Script < Config::Pattern
 
       desc "Name of the script"
@@ -32,7 +63,7 @@ module Config
       desc "Code to determine if this script should be run"
       attr :not_if, nil
 
-      desc "The not_if used command to execute not_if"
+      desc "The command used to execute not_if"
       attr :not_if_exec, nil
 
       desc "Arguments passed to not_if_exec"
