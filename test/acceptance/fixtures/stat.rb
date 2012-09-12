d2dcca71795b05956889910c539269b3cfc6575d
Ryan Carver <ryan@ryancarver.com>
2012-09-11 19:37:19 -0700

    better stat test names

--- test/acceptance/fixtures/stat.rb
+++ test/acceptance/fixtures/stat.rb
@@ -1,20 +1,19 @@
-file "/tmp/file-test-chmod" do |f|
+file "/tmp/test-file-chmod" do |f|
   f.content = "hello"
   f.mode = 0777
 end
 
-dir "/tmp/dir-test-chmod" do |f|
-  #f.mode = 01777
+dir "/tmp/test-dir-chmod" do |f|
+  f.mode = 01777
 end
 
-file "/tmp/file-test-chown" do |f|
+file "/tmp/test-file-chown" do |f|
   f.content = "hello"
   f.owner = "vagrant"
   f.group = "admin"
 end
 
-dir "/tmp/dir-test-chown" do |f|
+dir "/tmp/test-dir-chown" do |f|
   f.owner = "vagrant"
   f.group = "admin"
 end
-
