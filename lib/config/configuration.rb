c12eed66d5c63a1e624dc793be336729f4da1147
Ryan Carver <ryan@ryancarver.com>
2012-12-28 23:32:44 -0500

    reimplement configuration logging with Levels

--- lib/config/configuration.rb
+++ lib/config/configuration.rb
@@ -8,8 +8,49 @@ module Config
 
     # Internal: Create a merge from one or more configuration levels.
     def self.merge(*levels)
-      Levels::Merged.new(levels)
+      merged = Levels::Merged.new(levels)
+      merged.event_handler = EventHandler.new
+      merged
     end
+
+    class EventHandler
+      include Config::Core::Loggable
+
+      def initialize
+        @base_color = :magenta
+        @alt_color  = :cyan
+      end
+
+      def on_values(values)
+
+        if values.size == 1
+          final = values.final
+          log << log.colorize("Read #{values.group_key}.#{values.value_key} => #{final.inspect} from #{final.level_name}", @base_color)
+        else
+          log << log.colorize("Read #{values.group_key}.#{values.value_key}", @base_color)
+          log.indent do
+            values.each do |value|
+              values.notify(self)
+              if value.final?
+                word = "Use "
+                color = @base_color
+              else
+                word = "Skip"
+                color = @alt_color
+              end
+              log << log.colorize("#{word} #{value.inspect} from #{value.level_name}", color)
+            end
+          end
+        end
+      end
+
+      def on_nested_values(values)
+        log.indent do
+          on_values(values)
+        end
+      end
+    end
+
     # Enables dot syntax for levels and groups.
     module MethodMissing
       def method_missing(message, *args, &block)
